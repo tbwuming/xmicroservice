d05b3866e83ceca2a2164f7e4ad87de312a21ec6
wuming <376511022@qq.com>
2018-03-22 11:13:45 +0800

    添加测试

---
+++ usercenter/src/main/java/com/usercenter/UserCenterApplication.java
@@ -0,0 +1,12 @@
+package com.usercenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserCenterApplication.class, args);
+	}
+}
---
+++ usercenter/src/test/java/com/usercenter/controller/HelloControllerTest.java
@@ -0,0 +1,36 @@
+package com.usercenter.controller;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT) // , classes=UserCenterApplication.class)
+public class HelloControllerTest {
+
+	private MockMvc mvc;
+
+	@Before
+	public void setUp() throws Exception {
+		mvc = MockMvcBuilders.standaloneSetup(new HelloController()).build();
+	}
+
+	@Test
+	public void test() throws Exception {
+		mvc.perform(MockMvcRequestBuilders.get("/hello").accept(MediaType.APPLICATION_JSON)).andDo(print())
+				.andExpect(status().isOk()).andExpect(content().string(equalTo("Hello World")));
+
+	}
+
+}
